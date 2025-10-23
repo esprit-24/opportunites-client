3609eb34f88aac360e4f4c50aaeca35749f696d5
Yaya Gaye <gaye.yaya@ugb.edu.sn>
2025-10-23 21:37:40 +0000

    modification_des_eltsdynamique

--- src/app/Candidats/candidat-dashboard/candidat-dashboard.component.ts
+++ src/app/Candidats/candidat-dashboard/candidat-dashboard.component.ts
@@ -11,6 +11,8 @@ import { Domaine } from '../../models/domaine.model';
 import { Region } from '../../models/region.model';
 import { DomaineService } from '../../services/domaine.service';
 import { OpportuniteService } from '../../services/opportunite.service';
+import { Organisation } from '../../models/organisation.model';
+import { OrganisationService } from '../../services/organisation.service';
 
 
 @Component({
@@ -30,6 +32,13 @@ export class CandidatDashboardComponent implements OnInit {
   regions: Region[] = [];
   villes: Ville[] = [];
 
+  totalOpportunites: number = 0;
+  displayedNumber: number = 0; // Pour l'animation
+
+  displayedOrganisations: number = 0;
+  organisations: Organisation[] = [];
+  totalOrganisations: number = 0;
+
 
 
   typeContrats: string[] = Object.values(TypeContrat);
@@ -39,7 +48,8 @@ export class CandidatDashboardComponent implements OnInit {
     private authService: AuthService,
     private villeService: VilleService,
     private domaineService: DomaineService,
-    private opportuniteService: OpportuniteService
+    private opportuniteService: OpportuniteService,
+    private organisationService: OrganisationService
   )
   {}
 
@@ -84,21 +94,70 @@ export class CandidatDashboardComponent implements OnInit {
     this.router.navigate(['/login']);
   }
 
-  
+
   loadOpportunites(): void {
-    this.opportuniteService.getAllOpportunites().subscribe({
-      next: (data: Opportunite[]) => {
-        // Filtrer pour ne garder que les opportunités actives
-        const actives = data.filter(o => o.statut === 'ACTIVE');
+      this.opportuniteService.getAllOpportunites().subscribe({
+        next: (data: Opportunite[]) => {
+          // Filtrer pour ne garder que les opportunités actives
+          const actives = data.filter(o => o.statut === 'ACTIVE');
 
-        this.opportunites = actives.slice(0, 2);
-      },
+          this.opportunites = actives.slice(0, 2);
+          this.totalOpportunites = actives.length;   // Pour le compteur total
+          this.startCounter();
+        },
 
-      error: (error: any) => {
-        console.error('Erreur lors de la récupération des opportunités:', error);
+        error: (error: any) => {
+          console.error('Erreur lors de la récupération des opportunités:', error);
+        }
+      });
+    }
+
+    startCounter(): void {
+    let count = 0;
+    const interval = setInterval(() => {
+      count++;
+      this.displayedNumber = count;
+      if (count >= this.totalOpportunites) {
+        clearInterval(interval);
       }
-    });
+    }, 50); // vitesse de l'animation
   }
 
-    
+ // Organisations
+loadOrganisations(): void {
+  this.organisationService.getAllOrganisations().subscribe({
+    next: (data: Organisation[]) => {
+      this.organisations = data;
+      this.totalOrganisations = data.length;
+
+      // Lancer l'animation seulement si total > 0
+      if (this.totalOrganisations > 0) {
+        this.startOrganisationsCounter();
+      }
+    },
+    error: (err) => console.error(err)
+  });
+}
+
+startOrganisationsCounter(): void {
+  this.displayedOrganisations = 0;
+  const duration = 1000; // durée de l'animation en ms
+  const stepTime = Math.floor(duration / this.totalOrganisations);
+  let count = 0;
+
+  const interval = setInterval(() => {
+    count++;
+    this.displayedOrganisations = count;
+    if (count >= this.totalOrganisations) {
+      clearInterval(interval);
+    }
+  }, stepTime);
+}
+
+
+
+
+
+
+
 }
